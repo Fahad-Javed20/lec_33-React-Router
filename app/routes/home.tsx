6bb3de94aeb85f7df834a230a45d5f9ce4cd1cb0
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2026-01-03 23:02:56 +0500

    make routes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,10 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import HeaderComponent from "~/components/HeaderComponent";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+export default function HomePage() {
+  return (
+    <div>
+      <HeaderComponent/>
+    </div>
+  )
 }
 
-export default function Home() {
-  return <Welcome />;
-}
